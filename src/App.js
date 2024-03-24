79cec4c8b983bab72336858538bee47f92dabdcb
azimia <ra12372@auca.kg>
2024-03-25 01:04:41 +0600

    Header part done. Testing getting data from Weathermap. (Success)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {CloudSun} from "./Icons";
+
+let response = await fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=58dfaf7cc39a161eaa3828350388e50e");
+if(response.ok){
+}
+else {
+  alert(response.status);
+}
+setTimeout(()=> console.log(response.json()), 500);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className="flex justify-between items-stretch py-6 px-14">
+        <div className="flex flex-col justify-between">
+          <div className="flex flex-col gap-10">
+            <h1 className="text-white font-medium text-2xl flex gap-2 "><i className="fa-solid fa-location-dot"></i>New York<i
+                className="fa-solid fa-chevron-right self-center"></i></h1>
+            <span className="text-white font-medium text-5xl">Cloudy</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="Celsius text-white font-medium text-6xl">26</span>
+            <span className="text-white text-lg">Sunday | 12 Dec 2023</span>
+          </div>
+        </div>
+        <CloudSun></CloudSun>
       </header>
-    </div>
+    </main>
   );
 }
 
