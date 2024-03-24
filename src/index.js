8e39317a55cc660a3a832bb72ccd47744a2be0e9
azimia <ra12372@auca.kg>
2024-03-24 17:51:17 +0600

    Set up project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let a = fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=58dfaf7cc39a161eaa3828350388e50e");
+setTimeout(()=> console.log(a), 500);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
